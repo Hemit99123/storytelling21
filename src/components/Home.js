fec2fbc5da9956183b72c120fab8bb2480c84411
Hemit Patel <hemitpatel@computer4u.com>
2022-02-05 15:51:18 -0500

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -51,25 +51,7 @@ function App() {
     }
   };
 
-  const signInWithFaceBook = async () => {
-    try {
-      const res = await signInWithPopup(auth, provider2);
-      const user = res.user;
-      const q = query(collection(db, "users"), where("uid", "==", user.uid));
-      const docs = await getDocs(q);
-      if (docs.docs.length === 0) {
-        await addDoc(collection(db, "users"), {
-          uid: user.uid,
-          name: user.displayName,
-          authProvider: "facebook-provider",
-          email: user.email,
-        });
-      }
-    } catch (err) {
-      console.error(err);
-      alert(err.message);
-    }
-  };
+
 
 
 
@@ -145,7 +127,7 @@ const getPost = async () => {
     return (
       <div>
       <h1>Welcome {name}!</h1>
-      <h3>540 lines of code were written for this project!</h3>
+      <h3>500 lines of code were written for this project!</h3>
       <div className="homePage">
       <Button variant="secondary" onClick={signUserOut}>Sign Out</Button>
       <br />
