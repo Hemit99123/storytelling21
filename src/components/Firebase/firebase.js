6af9a0b7e17b4f51d8619adfb94febf3a7ca3298
Hemit Patel <hemitpatel@computer4u.com>
2022-02-06 09:06:44 -0500

    fdfd

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -6,13 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAUeZnvEoR09jy_UcrubVhz9_anHQoARes",
-    authDomain: "storytelling21-43842.firebaseapp.com",
-    projectId: "storytelling21-43842",
-    storageBucket: "storytelling21-43842.appspot.com",
-    messagingSenderId: "560010805607",
-    appId: "1:560010805607:web:70b1faee6b4105a56fefc2",
-    measurementId: "G-R7G8Q47654"
+    apiKey: "AIzaSyDh7lpH4-atZ1nNXOrlgxJLockCEi-_2Sc",
+    authDomain: "storytelling-edf7e.firebaseapp.com",
+    projectId: "storytelling-edf7e",
+    storageBucket: "storytelling-edf7e.appspot.com",
+    messagingSenderId: "967786761331",
+    appId: "1:967786761331:web:af3e3190e9b0400869ae06"
 };
 
 // Initialize Firebase
